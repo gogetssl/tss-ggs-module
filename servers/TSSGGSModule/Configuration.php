b36cf07946b48a8ece722a851932d291734017d0
marcinkutamg <marcin.ku@modulesgarden.com>
2025-09-16 13:06:03 +0200

    Update - version number changed to 1.1.0

--- servers/TSSGGSModule/Configuration.php
+++ servers/TSSGGSModule/Configuration.php
@@ -12,7 +12,7 @@ class Configuration extends AbstractConfiguration
     public $description = '';
     public $clientareaName = 'The SSL Store & GoGetSSL Module';
     public $encryptHash = 'uUc1Y8cWxDOGaby11lBwelqzo6PGMTA0dbHaKQ109olufoJgIFBOgmReKCZbpCYpASnrtfjmCIUyplaBJaUh40auDALprOHtj1g92ZRBS6S94IbZWaeZRYkG1f81h6qLMYEOr016RurCnmodFCWdMkTqrlVBvH249gzXPduKQVXpN9hooComaRPY5jZD6s8GdfR5E_BNP3v8Ui8RrdqMPST_8quMW48LhHY88xCvSWwDNjkC2tCAaK67Id2NjzIdoNTHUMISRg81nHX8ZGcbP74mxixo_ASd8YoWnDCAs8yiT4t0PwKRO_y3C1kDo69Nxz1YYt4tY1VzOD_DFBulAA5NCJLfogroo';
-    public $version = '1.0.3';
+    public $version = '1.1.0';
     public $author = 'ModulesGarden';
 
     public $tablePrefix = 'TSSGGSModule_';
